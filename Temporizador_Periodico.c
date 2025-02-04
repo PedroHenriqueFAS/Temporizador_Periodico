035fe8839394cd2205f35d16fb0df23c37e95a55
Pedro <pedrohenriquefa97@gmail.com>
2025-02-03 21:06:54 -0300

    Adição de comentários

--- Temporizador_Periodico.c
+++ Temporizador_Periodico.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "pico/stdlib.h"
-#include "hardware/timer.h"
+#include "pico/stdlib.h"     // Inclui a biblioteca padrão para funcionalidades básicas como GPIO, temporização e comunicação serial.
+#include "hardware/timer.h"  // Inclui a biblioteca para gerenciamento de temporizadores de hardware.
 
 #define LED_PIN_RED 11
 #define LED_PIN_YELLOW 12
@@ -8,8 +8,10 @@
 
 int estado = 0;  // Controla o estado atual do semáforo
 
-// Callback do temporizador
+// Função de callback que será chamada repetidamente pelo temporizador
+// O tipo bool indica que a função deve retornar verdadeiro ou falso para continuar ou parar o temporizador.
 bool repeating_timer_callback(struct repeating_timer *t) {
+    // Imprime uma mensagem na saída serial indicando que 3 segundo se passou.
     printf("3 segundos passaram\n");
 
     // Desliga todos os LEDs antes de ativar o próximo estado
@@ -33,6 +35,7 @@ bool repeating_timer_callback(struct repeating_timer *t) {
 }
 
 int main() {
+    // Inicializa a comunicação serial, permitindo o uso de funções como printf.
     stdio_init_all();
 
     // Inicializa os pinos dos LEDs
@@ -43,9 +46,14 @@ int main() {
     gpio_init(LED_PIN_GREEN);
     gpio_set_dir(LED_PIN_GREEN, true);
 
+    // Declaração de uma estrutura de temporizador de repetição.
+    // Esta estrutura armazenará informações sobre o temporizador configurado.
     struct repeating_timer timer;
+
+    // Configura o temporizador para chamar a função de callback a cada 3 segundo.
     add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
+    // Loop infinito que mantém o programa em execução.
     while (true) {
         sleep_ms(1000);
         printf("+1\n");
